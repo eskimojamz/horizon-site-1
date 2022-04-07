2503119c8c4d26a1579e096de42e8e6da6d94439
James Kim <juleskondo@gmail.com>
2022-04-07 18:40:09 +0900

    initial commit

---
+++ horizon-site/components/plasmic/blank_project/icons/PlasmicIcon__Call.jsx
@@ -0,0 +1,37 @@
+// @ts-nocheck
+/* eslint-disable */
+/* tslint:disable */
+/* prettier-ignore-start */
+import React from "react";
+import { classNames } from "@plasmicapp/react-web";
+
+export function CallIcon(props) {
+  const { className, style, title, ...restProps } = props;
+  return (
+    <svg
+      xmlns={"http://www.w3.org/2000/svg"}
+      fill={"none"}
+      viewBox={"0 0 24 24"}
+      height={"1em"}
+      width={"1em"}
+      style={{
+        fill: "currentcolor",
+        ...(style || {}),
+      }}
+      className={classNames("plasmic-default__svg", className)}
+      {...restProps}
+    >
+      {title && <title>{title}</title>}
+
+      <path
+        d={
+          "M20.01 15.38c-1.23 0-2.42-.2-3.53-.56a.977.977 0 00-1.01.24l-1.57 1.97c-2.83-1.35-5.48-3.9-6.89-6.83l1.95-1.66c.27-.28.35-.67.24-1.02-.37-1.11-.56-2.3-.56-3.53 0-.54-.45-.99-.99-.99H4.19C3.65 3 3 3.24 3 3.99 3 13.28 10.73 21 20.01 21c.71 0 .99-.63.99-1.18v-3.45c0-.54-.45-.99-.99-.99z"
+        }
+        fill={"currentColor"}
+      ></path>
+    </svg>
+  );
+}
+
+export default CallIcon;
+/* prettier-ignore-end */
---
+++ horizon-site/pages/index.jsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { PlasmicHomepage } from "../components/plasmic/blank_project/PlasmicHomepage";
+
+function Homepage() {
+  return <PlasmicHomepage />;
+}
+
+export default Homepage;
